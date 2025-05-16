57ef314777c45b39f0c96d6439f9779b8edb0c82
lq-wq <61339326+lq-wq@users.noreply.github.com>
2025-05-16 15:21:32 +0800

    Update diy-part.sh

--- build/Official/diy-part.sh
+++ build/Official/diy-part.sh
@@ -65,6 +65,7 @@ git clone https://github.com/lq-wq/luci-app-quickstart.git package/luci-app-quic
 git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
 git clone https://github.com/sirpdboy/luci-app-advancedplus.git luci-app-advancedplus     # 酷猫主题设置
+git clone https://github.com/sirpdboy/luci-app-watchdog package/watchdog                  # 看门狗
 
 # 晶晨CPU系列打包固件设置(不懂请看说明)
 export amlogic_model="s905d"
